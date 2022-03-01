0ccdc6301b4a4bee963aaf530be377ed363d8974
Julia Sawka <sawka.julia@gmail.com>
2022-03-01 08:05:32 +0100

    add it

---
+++ js/script.js
@@ -0,0 +1,35 @@
+'use strict';
+'use strict';
+
+function titleClickHandler(){
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+}
+
+const buttonTest = document.getElementById('button-test');
+
+buttonTest.addEventListener('click', titleClickHandler);
+
+'use strict';
+
+function titleClickHandler(event){
+  console.log('Link was clicked!');
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+console.log(event)}
