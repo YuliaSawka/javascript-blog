2ed57f777a7cc5313ab66ff1d10bb35732763217
Julia Sawka <sawka.julia@gmail.com>
2022-03-02 23:01:01 +0100

    adding

--- js/script.js
+++ js/script.js
@@ -16,10 +16,19 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+  const activeArticles = document.querySelectorAll('.titles a.active');
 
+  for(let activeArticle of activeactiveArticles){
+    activeArticle.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
